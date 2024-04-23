867af19127ed630dd9062eabc4e113e09f4cd233
Logan Bechtel <bechtello25@spaldinghs.org>
2024-04-23 13:22:31 -0400

    Fix: mispell

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Route for the root page
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '', 'publicn/html/index.html'));
+    res.sendFile(path.join(__dirname, '', 'public/html/index.html'));
 });
 
 app.listen(port, () => {
