28c63fb35d562a46204ad5c0532e5319413f644d
Logan Bechtel <bechtello25@spaldinghs.org>
2024-04-25 08:58:31 -0400

    Update: home adjectives.

--- public/js/type.js
+++ public/js/type.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
     var titles = [
-        "Software Developer",
+        "Software Engineer",
         "Java Programmer",
-        "Engineer",
+        "Team Leader",
         "Web Developer",
-        "JavaScript Programmer",
+        "FIRST Team Member",
         "Open Source Contributor"
     ];
 
