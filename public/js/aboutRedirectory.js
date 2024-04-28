f22aa7cb3affa781d9f31732939edd97430dc861
BechtelLo25 <91638125+BechtelLo25@users.noreply.github.com>
2024-04-28 13:06:42 -0400

    Feat: Image redirectory and animation and minor fixes

---
+++ public/js/aboutRedirectory.js
@@ -0,0 +1,49 @@
+document.getElementById("PixelBorderWithJava").addEventListener("click", function() {
+
+    window.open("https://dev.java/", "_blank");
+  });
+
+document.getElementById("PixelBorderWithHtml").addEventListener("click", function() {
+
+    window.open("https://html.spec.whatwg.org/", "_blank");
+  });
+
+document.getElementById("PixelBorderWithJs").addEventListener("click", function() {
+
+    window.open("https://www.javascript.com/", "_blank");
+  });
+
+document.getElementById("PixelBorderWithGit").addEventListener("click", function() {
+
+    window.open("https://git-scm.com/", "_blank");
+  });
+
+document.getElementById("PixelBorderWithCss").addEventListener("click", function() {
+
+    window.open("https://www.w3.org/TR/CSS/#intro", "_blank");
+  });
+
+document.getElementById("PixelBorderWithNode").addEventListener("click", function() {
+
+    window.open("https://nodejs.org/en", "_blank");
+  });
+
+document.getElementById("PixelBorderWithVSCode").addEventListener("click", function() {
+
+    window.open("https://code.visualstudio.com/", "_blank");
+  });
+
+document.getElementById("PixelBorderWithEclipse").addEventListener("click", function() {
+
+    window.open("https://eclipseide.org/", "_blank");
+  });
+
+document.getElementById("PixelBorderWithMS").addEventListener("click", function() {
+
+    window.open("https://www.microsoft.com/en-us/windows/?r=1", "_blank");
+  });
+
+document.getElementById("PixelBorderWithGithub").addEventListener("click", function() {
+
+    window.open("https://github.com/", "_blank");
+  });
